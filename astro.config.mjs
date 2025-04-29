9080e4eb3f069c91949fc8cc5e87f2d2ae6f25de
Hugo <hugo.carro.gomez@ieslaarboleda.es>
2025-04-29 10:58:31 +0200

    Ultimas modi

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			customCss: ['./src/styles/global.css'],
+			customCss: ['./src/styles/custom.css'],
 			title: '',
 			logo: {
 				src: './src/assets/floogos_dark.png',
